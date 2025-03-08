830cf2af45b78d00569496961d3e11388334ad01
shambhavi0608 <148109614+shambhavi0608@users.noreply.github.com>
2025-03-08 16:34:35 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+sqlite3.paramstyle = 'named'
+
+root = Tk()
+root.title("Login/Signup Form")
+root.geometry("400x400")
+root.resizable(width=False, height=False)
+lframe = LabelFrame(root, padx = 15, pady = 15, borderwidth=5)
+sframe = LabelFrame(root, padx=15, pady=15, borderwidth=5)
